d6029eacef00e77aa65aa93fb7e8eaa2cb00da0c
FerMod <tudelaigarcia@gmail.com>
2017-12-17 18:21:49 +0100

    Add image transmision

--- aplicacion_serie/transmit.c
+++ aplicacion_serie/transmit.c
@@ -11,18 +11,22 @@
 /* Prepara la transmision de la imagen                         */
 /***********************************************************************************/
 
-void preparar_transmision(imagen in_imagen)
-{
+void preparar_transmision(imagen in_imagen) {
 
-//////////////////////////////////////////////////////////////////////////////////////
-// POR HACER: 
-// codigo para preparar la transmision de la imagen
-// a cada pixel hay que aplicarle la funcion transmit_aux
-//
-// tened en cuenta la cabecera de la funcion transmit_aux.h
-// extern void transmit_aux (unsigned char vp1, int i, int j, int h, int w);
-// pixel de la imagen, su posicion (i,j) y dimensiones de la imagen (h,w)
-//////////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////////////
+    // codigo para preparar la transmision de la imagen
+    // a cada pixel hay que aplicarle la funcion transmit_aux
+    //
+    // tened en cuenta la cabecera de la funcion transmit_aux.h
+    // extern void transmit_aux (unsigned char vp1, int i, int j, int h, int w);
+    // pixel de la imagen, su posicion (i,j) y dimensiones de la imagen (h,w)
+    //////////////////////////////////////////////////////////////////////////////////////
+
+    int i, j;
+    for(i = 0; i < in_imagen.h; i++) {
+        for(j = 0; j < in_imagen.w; j++) {
+            transmit_aux(in_imagen.im[i][j], i, j, in_imagen.h, in_imagen.w);
+        }
+    }
 
 }
-
